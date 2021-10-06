27b776d25861f0d4e7452364addb01453dc681db
Abhishek Kulkarni <kul.abhishake@gmail.com>
2021-10-06 10:42:16 +0530

    logging file added

--- and.py
+++ and.py
@@ -13,7 +13,7 @@ import logging
 import os
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
-log_dir = "perceptron logs"
+log_dir = "logs"
 os.makedirs(log_dir, exist_ok= True)
 logging.basicConfig(filename = os.path.join(log_dir,"running_logs.log"),level= logging.INFO, format= logging_str, filemode= "a")
 
